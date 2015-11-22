e762f7d2c06d8a8e39b139a3c045565c9c0e266e
Jonathan Gros-Dubois <grosjona@yahoo.com.au>
2015-11-22 17:32:18 +1100

    Can now provide options object to verifyToken method

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ var jwt = require('jsonwebtoken');
 
 var AuthEngine = function () {};
 
-AuthEngine.prototype.verifyToken = function (encryptedToken, key, callback) {
+AuthEngine.prototype.verifyToken = function (encryptedToken, key, options, callback) {
   if (typeof encryptedToken == 'string' || encryptedToken == null) {
-    jwt.verify(encryptedToken || '', key, callback);
+    jwt.verify(encryptedToken || '', key, options, callback);
   } else {
     var err = new Error('Invalid token format - Token must be a string');
     err.name = "TokenFormatError";
