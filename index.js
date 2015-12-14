987527d6b2a938b14a24ec7d5f7afebabf6a0ef5
Jonathan Gros-Dubois <grosjona@yahoo.com.au>
2015-12-14 21:09:03 +1100

    Renamed encryptedToken to signedToken

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ var jwt = require('jsonwebtoken');
 
 var AuthEngine = function () {};
 
-AuthEngine.prototype.verifyToken = function (encryptedToken, key, options, callback) {
-  if (typeof encryptedToken == 'string' || encryptedToken == null) {
-    jwt.verify(encryptedToken || '', key, options, callback);
+AuthEngine.prototype.verifyToken = function (signedToken, key, options, callback) {
+  if (typeof signedToken == 'string' || signedToken == null) {
+    jwt.verify(signedToken || '', key, options, callback);
   } else {
     var err = new Error('Invalid token format - Token must be a string');
     err.name = "TokenFormatError";
