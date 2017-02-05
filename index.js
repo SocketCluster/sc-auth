b2d9dc511fe1dae74942f9527e2ce8e6e7ca4ba9
Jonathan Gros-Dubois <grosjona@yahoo.com.au>
2017-02-05 17:48:42 +0000

    Auth enigne now support both async or sync verification and signing

--- index.js
+++ index.js
@@ -3,12 +3,29 @@ var jwt = require('jsonwebtoken');
 var scErrors = require('sc-errors');
 var InvalidArgumentsError = scErrors.InvalidArgumentsError;
 
-
 var AuthEngine = function () {};
 
 AuthEngine.prototype.verifyToken = function (signedToken, key, options, callback) {
+  var jwtOptions = cloneObject(options);
+  delete jwtOptions.async;
   if (typeof signedToken == 'string' || signedToken == null) {
-    jwt.verify(signedToken || '', key, options, callback);
+
+    if (options.async) {
+      jwt.verify(signedToken || '', key, jwtOptions, callback);
+    } else {
+      var err = null;
+      var token;
+      try {
+        token = jwt.verify(signedToken || '', key, jwtOptions);
+      } catch (error) {
+        err = error;
+      }
+      if (err) {
+        callback(err);
+      } else {
+        callback(null, token);
+      }
+    }
   } else {
     var err = new InvalidArgumentsError('Invalid token format - Token must be a string');
     callback(err);
@@ -16,7 +33,23 @@ AuthEngine.prototype.verifyToken = function (signedToken, key, options, callback
 };
 
 AuthEngine.prototype.signToken = function (token, key, options, callback) {
-  jwt.sign(token, key, options, callback);
+  var jwtOptions = cloneObject(options);
+  delete jwtOptions.async;
+
+  if (options.async) {
+    jwt.sign(token, key, jwtOptions, callback);
+  } else {
+    var signedToken = jwt.sign(token, key, jwtOptions);
+    callback(null, signedToken);
+  }
 };
+
+function cloneObject(object) {
+  var clone = {};
+  Object.keys(object || {}).forEach(function (key) {
+    clone[key] = object[key];
+  });
+  return clone;
+}
 
 module.exports.AuthEngine = AuthEngine;
